0b005193fdb52bf65819465b692ae55a7bbb82a2
miansen <1158827539@qq.com>
2019-09-27 17:44:29 +0800

    高可用的服务注册中心

---
+++ spring-cloud-eureka-server-hign/src/main/java/org/spring/cloud/eureka/server/hign/EurekaServerHignApplication.java
@@ -0,0 +1,14 @@
+package org.spring.cloud.eureka.server.hign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerHignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerHignApplication.class, args);
+	}
+}
