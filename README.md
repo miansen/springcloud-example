6b370a2346a454725c3575dad3acc6957a75e390
miansen <1158827539@qq.com>
2019-11-12 18:05:12 +0800

    add spring-cloud-gateway

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [二、Spring Cloud-服务注册中心 Eureka](https://miansen.wang/2019/09/24/spring-cloud-eureka)
 
-[三、Spring Cloud-负载均衡 Ribbon）](https://miansen.wang/2019/09/25/spring-cloud-ribbon)
+[三、Spring Cloud-负载均衡 Ribbon](https://miansen.wang/2019/09/25/spring-cloud-ribbon)
 
 [四、Spring Cloud-接口调用 Feign](https://miansen.wang/2019/09/26/spring-cloud-feign)
 
@@ -13,5 +13,7 @@
 [五、SpringCloud-服务容错 Hystrix（断路器）](https://miansen.wang/2019/09/30/spring-cloud-hystrix)
 
 [六、SpringCloud-路由器和过滤器 Zuul](https://miansen.wang/2019/10/12/spring-cloud-zuul)
+
+[七、Spring Cloud-网关 Spring-Cloud-Gateway](https://miansen.wang/2019/10/21/spring-cloud-gateway)
 
 [七、SpringCloud-服务配置中心 Spring-Cloud-Config](https://miansen.wang/2019/10/22/spring-cloud-config)
---
+++ spring-cloud-gateway/src/main/java/org/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package org.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	/**
+	 * 通过代码的形式配置路由
+	 * @param builder
+	 * @return
+	 */
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(
+						p -> p.path("/users/zhangsan").uri("https://www.baidu.com")
+						)
+				.build();
+	}
+}
